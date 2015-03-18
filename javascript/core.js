877421802f0c4c7fa6d505d0a314a11e30adfabf
Eivlys <vamcheryl@gmail.com>
2015-03-19 00:15:29 +0800

    完成申请页  修改风格

--- javascript/core.js
+++ javascript/core.js
@@ -1,29 +1,31 @@
-//buyCarService焦点图轮播效果
-    $(function() {
-        $('#slides').slidesjs({
-            play: {
-                active: false,
-                auto: true,
-                interval: 3500, //控制轮播间隔时间
-                swap: false,
-            },
-            navigation:{
-                active:false
-            },
-            pagination:{
-                active:false
-            }
-        });
-    });
+// //buyCarService焦点图轮播效果
+//     $(function() {
+//         $('#slides').slidesjs({
+//             play: {
+//                 active: false,
+//                 auto: true,
+//                 interval: 3500, //控制轮播间隔时间
+//                 swap: false,
+//             },
+//             navigation:{
+//                 active:false
+//             },
+//             pagination:{
+//                 active:false
+//             }
+//         });
+//     });
 
 //tab标签切换
 var tabIndex = 0;
 var tab = $('#tab_ls').find('a');
+var tabLi = $('#tab_ls').find('li');
 var tabCt = $('.car_content');
 tabCt.eq(1).hide();
+tabCt.eq(2).hide();
 tab.bind('click', function() {
     var index = tab.index(this);
-    $(this).addClass('cur').siblings('a').removeClass();
+    tabLi.eq(index).addClass('cur').siblings().removeClass('cur');
     console.log('add');
     tabCt.eq(index).show().siblings('.car_content').hide();
 });
