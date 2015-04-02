7ec82b33eaa0604ce6fa6e7155d5f9a86dcfcd3a
Eivlys <vamcheryl@gmail.com>
2015-04-03 03:37:02 +0800

    修改风格样式

--- javascript/core.js
+++ javascript/core.js
@@ -1,20 +1,65 @@
-// //buyCarService焦点图轮播效果
-//     $(function() {
-//         $('#slides').slidesjs({
-//             play: {
-//                 active: false,
-//                 auto: true,
-//                 interval: 3500, //控制轮播间隔时间
-//                 swap: false,
-//             },
-//             navigation:{
-//                 active:false
-//             },
-//             pagination:{
-//                 active:false
-//             }
-//         });
-//     });
+//buyCarService焦点图轮播效果
+$(function() {
+    $('#slides').slidesjs({
+        play: {
+            active: false,
+            auto: true,
+            interval: 3500, //控制轮播间隔时间
+            swap: false,
+        },
+        navigation:{
+            active:false
+        },
+        pagination:{
+            active:false
+        }
+    });
+});
+
+function resize(){
+    var slide = $(document.getElementsByClassName('slidesjs-container'));
+    var control = $(document.getElementsByClassName('slidesjs-control'));
+    var pre = $(document.getElementsByClassName('slidesjs-previous'));
+    var next = $(document.getElementsByClassName('slidesjs-next'));
+    slide.css({
+        width: '95%',
+        height: '200px',
+        margin:'0 auto'
+    });
+    control.css({
+        width: '100%',
+        height: '100%',
+        margin: '0 auto'
+    });
+    pre.text('上一张').css({
+        margin: '15px',
+        float: 'left',
+        color: '#f8801c',
+        textDecoration: 'none'
+    });
+    next.text('下一张').css({
+        margin: '15px',
+        float: 'right',
+        color: '#f8801c',
+        textDecoration: 'none'
+    });
+}
+// //carInfo图片展示
+$(function() {
+    $('#car_pic').slidesjs({
+        play: {
+            active: false,
+            auto: false,
+            swap: true,
+        },
+        navigation:{
+            active:true
+        },
+         pagination:{
+            active:false
+        }
+    });
+});
 
 //tab标签切换
 var tabIndex = 0;
